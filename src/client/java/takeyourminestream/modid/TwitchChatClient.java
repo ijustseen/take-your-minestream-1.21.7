0850177c6e01cbaabd0083d87c0e443b649a2900
CnoLvaN <anordgame@gmail.com>
2025-07-13 16:11:13 +0200

    Switch to raw IRC for Twitch & remove twitch4j

--- src/client/java/takeyourminestream/modid/TwitchChatClient.java
+++ src/client/java/takeyourminestream/modid/TwitchChatClient.java
@@ -1,52 +1,99 @@
 package takeyourminestream.modid;
 
-import com.github.twitch4j.TwitchClient;
-import com.github.twitch4j.TwitchClientBuilder;
-import com.github.twitch4j.chat.events.channel.ChannelMessageEvent;
 import takeyourminestream.modid.messages.MessageSpawner;
+import java.io.*;
+import java.net.Socket;
 import java.util.Random;
 import takeyourminestream.modid.ModConfig;
 
 public class TwitchChatClient {
-
-    private TwitchClient twitchClient;
+    private Socket socket;
+    private BufferedWriter writer;
+    private BufferedReader reader;
+    private Thread listenThread;
+    private volatile boolean running = false;
     private final Random random = new Random();
     private final MessageSpawner messageSpawner;
+    private String channelName;
 
     public TwitchChatClient(String channelName, MessageSpawner messageSpawner) {
+        this.channelName = channelName;
         this.messageSpawner = messageSpawner;
-        initializeTwitchClient();
+        connect();
     }
 
     private String getRandomColor() {
         return ModConfig.NICK_COLORS[random.nextInt(ModConfig.NICK_COLORS.length)];
     }
 
-    private void initializeTwitchClient() {
-        // For public chat reading, we generally don't need a client secret or access token
-        // if we are just joining a channel anonymously to read messages.
-        // However, if we need to send messages or use other Helix API features, we'd need proper auth.
-        this.twitchClient = TwitchClientBuilder.builder()
-            .withEnableChat(true)
-            .build();
+    private void connect() {
+        try {
+            socket = new Socket("irc.chat.twitch.tv", 6667);
+            writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-        twitchClient.getEventManager().onEvent(ChannelMessageEvent.class, event -> {
-            String message = event.getMessage();
-            String user = event.getUser().getName();
-            System.out.println("[" + event.getChannel().getName() + "] " + user + ": " + message);
-            // Here we will pass the message to MessageSpawner
-            // For now, we are just printing it to console for verification.
-            messageSpawner.setCurrentMessage(getRandomColor() + user + ": §r" + message);
-        });
+            // Анонимная авторизация (можно заменить на свой логин/токен при необходимости)
+            String nick = "justinfan" + (10000 + random.nextInt(90000));
+            sendRaw("PASS oauth:anonymous");
+            sendRaw("NICK " + nick);
+            sendRaw("JOIN #" + channelName.toLowerCase());
 
-        twitchClient.getChat().joinChannel(ModConfig.TWITCH_CHANNEL_NAME);
-        System.out.println("Joined Twitch channel: " + ModConfig.TWITCH_CHANNEL_NAME);
+            running = true;
+            listenThread = new Thread(this::listenLoop, "TwitchIRC-Listen");
+            listenThread.setDaemon(true);
+            listenThread.start();
+            System.out.println("[IRC] Connected to Twitch IRC as " + nick + ", joined #" + channelName);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("[IRC] Failed to connect to Twitch IRC");
+        }
+    }
+
+    private void sendRaw(String line) throws IOException {
+        writer.write(line + "\r\n");
+        writer.flush();
+    }
+
+    private void listenLoop() {
+        try {
+            String line;
+            while (running && (line = reader.readLine()) != null) {
+                if (line.startsWith("PING ")) {
+                    sendRaw("PONG " + line.substring(5));
+                } else if (line.contains("PRIVMSG")) {
+                    // Пример строки:
+                    // :username!username@username.tmi.twitch.tv PRIVMSG #channel :message text
+                    int excl = line.indexOf('!');
+                    int colon = line.indexOf(" :", 1);
+                    int hash = line.indexOf("#");
+                    if (excl > 1 && colon > 1 && hash > 1) {
+                        String user = line.substring(1, excl);
+                        String message = line.substring(colon + 2);
+                        String channel = line.substring(hash + 1, line.indexOf(' ', hash));
+                        System.out.println("[" + channel + "] " + user + ": " + message);
+                        messageSpawner.setCurrentMessage(getRandomColor() + user + ": §r" + message);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            if (running) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("[IRC] Disconnected from Twitch IRC");
     }
 
     public void disconnect() {
-        if (twitchClient != null) {
-            twitchClient.close();
-            System.out.println("Disconnected from Twitch chat.");
+        running = false;
+        try {
+            if (writer != null) sendRaw("PART #" + channelName.toLowerCase());
+        } catch (IOException ignored) {}
+        try {
+            if (socket != null) socket.close();
+        } catch (IOException ignored) {}
+        if (listenThread != null) {
+            try { listenThread.join(1000); } catch (InterruptedException ignored) {}
         }
+        System.out.println("[IRC] Disconnected from Twitch chat.");
     }
 } 
